0f457ead73edbe415e5529aa15f773569ac6c33c
Ryuma0724 <s21482804@nucba.ac.jp>
2023-11-08 16:36:56 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = ["0","1","2","3","4","5","6"]
+      wday_num = Date.today.wday
       if puts 
         wday_num >=7
         wday_num = wday_num -7
